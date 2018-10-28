200ce9c882851888df64615b3ba2d066b5fb633d
Sergei Dolgov <dolgovs@gmail.com>
2018-10-28 21:40:07 +0100

    Console typeclass

--- src/main/scala/com/example/client/ClientApp.scala
+++ src/main/scala/com/example/client/ClientApp.scala
@@ -4,6 +4,7 @@ import java.nio.file
 import java.nio.file.Paths
 import java.util.concurrent.Executors
 
+import cats.Monad
 import cats.effect._
 import cats.implicits._
 import fs2.{Pipe, Stream, hash}
@@ -30,26 +31,43 @@ object FileSystem {
   }
 }
 
+class Console[F[_] : Sync] {
+  def print(s: String): F[Unit] = Sync[F].delay {
+    println(s)
+  }
+}
+
+object Console {
+  def apply[F[_]](implicit F: Console[F]): Console[F] = F
+}
+
 object HttpClient {
 
-  def uploadFile[F[_] : Sync : ContextShift](path: file.Path)(client: Client[F]): F[String] = {
+  def upload[F[_] : Sync : ContextShift](path: file.Path)(client: Client[F]): F[String] = {
     val fileStream = FileSystem.read(path)
     for {
       sha <- Hashing.sha1(fileStream).compile.toVector
       shaString = sha.head
       req = Request[F](PUT, uri("https://httpbin.org/anything"), body = fileStream, headers = Headers(Header("sha", shaString)))
       r <- client.expect[String](req)
-      _ <- Sync[F].delay(println(r))
     } yield r
   }
 
 }
 
-object ClientSandbox extends IOApp with Http4sClientDsl[IO] {
+class UploadAndPrintResult[F[_] : Monad : ContextShift : ConcurrentEffect : Console] {
+  def run(path: file.Path): F[Unit] = for {
+    out <- BlazeClientBuilder[F](global).resource.use(HttpClient.upload[F](path))
+    p <- Console[F].print(out)
+  } yield ()
+}
+
+object ClientApp extends IOApp {
 
   def run(args: List[String]): IO[ExitCode] = {
     val path: file.Path = Paths.get("src/main/resources/logback.xml")
-    BlazeClientBuilder[IO](global).resource.use(HttpClient.uploadFile[IO](path)).as(ExitCode.Success)
+    implicit val c: Console[IO] = new Console[IO]
+    new UploadAndPrintResult[IO].run(path) as ExitCode.Success
   }
 
 }
